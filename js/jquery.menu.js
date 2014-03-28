3bf75e09bfbc4b30375e19ba5e5827e851bd4781
sfger <sfger111@163.com>
2014-03-28 17:47:55 +0800

    reich menu code functions

--- js/jquery.menu.js
+++ js/jquery.menu.js
@@ -4,8 +4,8 @@ if(!Function.prototype.bind){
 		if(typeof this!=="function"){
 			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
 		}
-		var aArgs = Array.prototype.slice.call(arguments, 1), 
-			fToBind = this, 
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+			fToBind = this,
 			fNOP    = function(){},
 			fBound  = function(){
 				return fToBind.apply(
@@ -33,13 +33,18 @@ $.fn.menu=function(options){
 		handler.prototype = {
 			init: function(box, options){
 				var that = this;
+				this.userOptions = options;
 				var document = window.document;
 				var getType = function(obj){ return toString.call(obj).slice(8, -1); };
-				box.addClass('menu-container').addClass('clearfix').hide();
-				$('<div style="position:relative;"><div class="menu-vertical-line"></div></div>').appendTo(box);;
+				box.addClass('menu-container clearfix').hide();
+				this.container = box.get(0);
 				this.menuitems = {};
 				var createMenu = function(data){
+					var wraper = document.createElement('div');
+					wraper.style.position = 'relative';
+					wraper.innerHTML = '<div class="menu-vertical-line"></div>';
 					var ul = document.createElement('ul');
+					ul.className = 'clearfix';
 					for(var i=0,ii=data.length-1; i<=ii; i++){
 						var li = document.createElement('li');
 						if(data[i]['class']=='separate'){
@@ -49,29 +54,109 @@ $.fn.menu=function(options){
 							var line = document.createElement('a');
 							var icon = document.createElement('span');
 							var text = document.createElement('span');
+							if(data[i].children && data[i].children.length){
+								var sub_tag = document.createElement('span');
+								sub_tag.className = "sub-tag";
+								line.appendChild(sub_tag);
+							}
 							icon.className = 'icon' + (data[i].icon ? ' icon-'+data[i].icon : '');
 							line.appendChild(icon);
 							text.innerHTML = data[i].text||'';
 							text.className = 'text';
 							line.appendChild(text);
 							line.setAttribute('href', 'javascript:;');
+							if(data[i].disabled) line.className = 'disabled';
 							if(data[i]['class']) li.className = data[i]['class'];
 							that.menuitems[data[i].name] = li;
 						}
+						line.option = data[i];
 						li.appendChild(line);
+						if(data[i].children && data[i].children.length){
+							var sub_container = document.createElement('div');
+							sub_container.style.display = 'none';
+							if(document.documentMode===5 || /MSIE 6/.test(navigator.userAgent)){
+								sub_container.style.position = 'absolute';
+							}else{
+								sub_container.style.position = 'relative';
+								sub_container.style.top = '-22px';
+							}
+							sub_container.style.left = '97px';
+							sub_container.appendChild(createMenu(data[i].children));
+							sub_container.className = 'menu-container clearfix';
+							li.appendChild(sub_container);
+						}
 						ul.appendChild(li);
 					}
-					return ul;
+					wraper.appendChild(ul);
+					return wraper;
 				};
-				var w = $(createMenu(options.data)).appendTo(box.find('>div'));
-				box.show();
-				if(document.documentMode===5 || /MSIE 6/.test(navigator.userAgent)){
-					box.find('.menu-vertical-line').css({height:w.height()+'px'});
-				}
+				var w = $(createMenu(options.data)).appendTo(box);
+				box.on({
+					mouseenter: function(){
+						if(this.showMenuTimer) clearTimeout(this.showMenuTimer);
+						this.showMenuTimer = null;
+					},
+					mouseleave: function(){
+						var that = this;
+						this.showMenuTimer = setTimeout(function(){
+							that.style.display = 'none';
+							that.showMenuTimer = null;
+						}, 300);
+					}
+				}).delegate('a', {
+					click: function(){
+						if(this.option.disabled) return false;
+						that.userOptions.onClick.bind(this)(this.option);
+					},
+					mouseenter: function(){
+						if(this.option.disabled) return true;
+						var sub = this.nextSibling;
+						if(sub){
+							if(sub.showMenuTimer) clearTimeout(sub.showMenuTimer);
+							sub.showMenu = true;
+							sub.style.display = '';
+							sub.showMenuTimer = null;
+						}
+					},
+					mouseleave: function(){
+						var sub = this.nextSibling;
+						if(sub && sub.showMenu===true){
+							sub.showMenuTimer = setTimeout(function(){
+								sub.showMenu = false;
+								sub.style.display = 'none';
+								sub.showMenuTimer = null;
+							}, 300);
+						}
+					}
+				}).delegate('.menu-container', {
+					mouseover: function(){
+						if(this.showMenuTimer) clearTimeout(this.showMenuTimer);
+						this.showMenu = true;
+						this.showMenuTimer = null;
+					},
+					mouseout: function(){
+						var that = this;
+						this.showMenuTimer = setTimeout(function(){
+							that.showMenu = false;
+							that.style.display = 'none';
+							that.showMenuTimer = null;
+						}, 300);
+					}
+				});
+				options.onCreate.bind(this)();
 			},
-			show: function(){
+			show: function(e){
+				$(this.container).css({left:e.pageX, top:e.pageY}).show();
+				if(document.documentMode===5 || /MSIE 6/.test(navigator.userAgent)){
+					$(this.container).find('.menu-vertical-line').each(function(){
+						this.style.height = this.parentNode.offsetHeight + 'px';
+					});
+				}
+				this.userOptions.onShow.bind(this)();
 			},
 			hide: function(){
+				$(this.container).hide();
+				this.userOptions.onHide.bind(this)();
 			},
 			/* option: [Object] just options.data object item
 			 * position: [String] before, after
@@ -81,11 +166,57 @@ $.fn.menu=function(options){
 			},
 			showMenuItem: function(name){
 			},
-			addMenuItem: function(option, position, target){
+			addMenuItem: function(option, position, refName){
+				var li = document.createElement('li');
+				if(option['class']=='separate'){
+					var line = document.createElement('span');
+					li.className = option['class'];
+				}else{
+					var line = document.createElement('a');
+					var icon = document.createElement('span');
+					var text = document.createElement('span');
+					icon.className = 'icon' + (option.icon ? ' icon-'+option.icon : '');
+					line.appendChild(icon);
+					text.innerHTML = option.text||'';
+					text.className = 'text';
+					line.appendChild(text);
+					line.setAttribute('href', 'javascript:;');
+					line.option = option;
+					if(option.disabled) line.className = 'disabled';
+					if(option['class']) li.className = option['class'];
+					this.menuitems[option.name] = li;
+				}
+				li.appendChild(line);
+				var $sc, method;
+				if(!refName){
+					$sc = $('ul', this.container).eq(0);
+					if(!position || position=='after'){
+						method = 'append';
+					}else{
+						method = 'prepend';
+					}
+				}else{
+					$sc = $(this.menuitems[refName]);
+					if(!position) position = after;
+					method = position;
+				}
+				$sc[method](li);
+				return this;
 			},
 			updateMenuItem: function(name, option){
+				if(!this.menuitems[name]) return this;
+				var $li = $(this.menuitems[name]);
+				$li.find('a')[option.disabled ? 'addClass' : 'removeClass']('disabled').get(0).option = option;
+				$li.find('.icon').removeClass().addClass('icon'+(option['icon'] ? ' icon-'+option['icon'] : ''));
+				$li.find('.text').html(option.text);
+				return this;
 			},
-			deleteMenuImte: function(){
+			deleteMenuItem: function(name){
+				if(!this.menuitems[name]) return this;
+				this.menuitems[name].option = null;
+				$(this.menuitems[name]).remove();
+				var data = this.userOptions.data
+				return this;
 			}
 		};
 		handler.prototype.init.prototype = handler.prototype;
